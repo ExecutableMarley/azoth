0ffe88999952855da42522090149896eb787efe2
Marley <marley.arns@yahoo.de>
2025-12-30 20:40:09 +0100

    refactor: Improved comments

--- Process/Core/MemoryCopy.hpp
+++ Process/Core/MemoryCopy.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <memory>
 
+#include "../Types/Address.hpp"
 #include "PointerEndpoint.hpp"
 
 namespace Azoth
@@ -18,185 +19,224 @@ typedef unsigned char BYTE;
 
 /**
  * @brief Cached copy of a contiguous remote memory region.
+ * 
+ * MemoryCopy provides a mechanism to copy a block of memory from a target
+ * process into a local buffer, operate on it locally, and optionally write
+ * it back to the target process.
+ * 
+ * @note No automatic synchronization is performed and the caller is responsible
+ * for explicitly calling readIn() and writeBack().
  */
 class MemoryCopy
 {
 public:
+    //Todo: Make constructor protected
 	MemoryCopy();
 	MemoryCopy(CMemoryModule* memory, uint64_t addr, size_t size);
 	MemoryCopy(CMemoryModule* memory, PointerEndpoint chainAddr, size_t size);
 
+    /**
+     * @brief Check whether this MemoryCopy instance is usable.
+     *
+     * @return True if a memory module, address, and buffer are properly set.
+     */
 	bool valid() const;
 
-	/**
-	* @brief Sets the memory source to an absolute address.
-	*
-	* @param[in] addr The absolute memory address to copy from.
-	*/
+    //==================================================================
+    // Source configuration
+    //==================================================================
+
+    /**
+     * @brief Set the memory source to an absolute address.
+     *
+     * @param[in] addr Absolute address in the target process.
+     */
 	void setAddress(uint64_t addr);
 
 	/**
-	* @brief Sets the memory source using a pointer chain.
-	*
-	* @param[in] chainAddr The resolved endpoint of a pointer chain.
-	*/
+     * @brief Set the memory source using a pointer-chain endpoint.
+     *
+     * @param[in] ptrEndpoint Endpoint of a pointer chain.
+     */
 	void setAddress(PointerEndpoint ptrEndpoint);
 
-	/**
-		* @brief Retrieves the address value stored in the `_chainAddr` member variable.
-		*
-		* @tparam T The type of the pointer value to be returned. Defaults to `uint64_t`.
-		* @return The pointer value stored in `_chainAddr`.
-		*/
+    /**
+     * @brief Retrieve the resolved base address used for copying.
+     *
+     * @tparam T Returned integer type (defaults to uint64_t).
+     * @return Base address in the target process.
+     */
 	template<typename T = uint64_t>
-	uint64_t getBaseAddress() const
+	T getBaseAddress() const
 	{
 		return _chainAddr.get();
 	}
 
-	/**
-		* @brief Resizes the internal buffer for the memory copy operation.
-		*
-		* @param[in] newSize The new size of the buffer in bytes.
-		*
-		* @note This method may reallocate the internal buffer.
-		*/
+    //==================================================================
+    // Buffer management
+    //==================================================================
+
+    /**
+     * @brief Resize the internal buffer.
+     *
+     * @param[in] newSize New buffer size in bytes.
+     *
+     * @note This may reallocate the internal buffer. However in this case the content is 
+     * carried over
+     */
 	void resize(size_t newSize);
 
-	/**
-		* @brief Reads data into the internal buffer using the currently set memory source.
-		*
-		* @return `true` if the read operation succeeds, `false` otherwise.
-		*
-		* @note The memory source must be set before calling this method.
-		*/
+    //==================================================================
+    // Read from target process
+    //==================================================================
+
+    /**
+     * @brief Read remote memory into the internal buffer.
+     *
+     * Uses the currently configured memory source.
+     *
+     * @return True if the read succeeds.
+     */
 	bool readIn();
 
-	/**
-		* @brief Reads data into the internal buffer from an absolute address.
-		*
-		* @param[in] addr The absolute memory address to read from.
-		* @param[in] size The number of bytes to read. If `0`, the currently set size is used.
-		* @return `true` if the read operation succeeds, `false` otherwise.
-		*/
+    /**
+     * @brief Read remote memory from an absolute address.
+     *
+     * @param[in] addr Base address in the target process.
+     * @param[in] size Number of bytes to read (0 = use current size).
+     *
+     * @return True if the read succeeds.
+     */
 	bool readIn(uint64_t addr, size_t size = 0);
 
-	/**
-		* @brief Reads data into the internal buffer from a pointer chain.
-		*
-		* @param[in] chainAddr The resolved endpoint of a pointer chain.
-		* @param[in] size The number of bytes to read. If `0`, the currently set size is used.
-		* @return `true` if the read operation succeeds, `false` otherwise.
-		*/
+    //Todo: might be obsolete
+    /**
+     * @brief Read remote memory from a pointer-chain endpoint.
+     *
+     * @param[in] ptrEndpoint Resolved endpoint of a pointer chain.
+     * @param[in] size        Number of bytes to read (0 = use current size).
+     *
+     * @return True if the read succeeds.
+     */
 	bool readIn(PointerEndpoint ptrEndpoint, size_t size = 0);
 
-	/**
-		* @brief Writes the internal buffer back to the currently set memory address.
-		*
-		* @return `true` if the write operation succeeds, `false` otherwise.
-		*
-		* @note The memory source must be set before calling this method.
-		*/
+    //==================================================================
+    // Write back to target process
+    //==================================================================
+
+    /**
+     * @brief Write the internal buffer back to the configured memory source.
+     *
+     * @return True if the write succeeds.
+     */
 	bool writeBack();
 
-	/**
-		* @brief Writes the internal buffer to a specified absolute address.
-		*
-		* @param[in] addr The absolute memory address to write to.
-		* @return `true` if the write operation succeeds, `false` otherwise.
-		*/
+    /**
+     * @brief Write the internal buffer to an absolute address.
+     *
+     * @param[in] addr Destination address in the target process.
+     *
+     * @return True if the write succeeds.
+     */
 	bool writeBack(uint64_t addr);
 
-	/**
-		* @brief Writes the internal buffer to a specified pointer chain endpoint.
-		*
-		* @param[in] chainAddr The resolved endpoint of a pointer chain.
-		* @return `true` if the write operation succeeds, `false` otherwise.
-		*/
+    /**
+     * @brief Write the internal buffer to a pointer-chain endpoint.
+     *
+     * @param[in] ptrEndpoint Resolved endpoint of a pointer chain.
+     *
+     * @return True if the write succeeds.
+     */
 	bool writeBack(PointerEndpoint ptrEndpoint);
 
-	/**
-		* @brief Retrieves a pointer to the internal buffer.
-		*
-		* @return A pointer to the internal buffer containing the copied memory.
-		*/
+    //==================================================================
+    // Buffer access
+    //==================================================================
+
+    /**
+     * @brief Get a pointer to the internal buffer.
+     *
+     * @return Pointer to the locally cached memory.
+     */
 	BYTE* getBuffer() const { return this->_buffer.get(); }
 
-	/**
-		* @brief Implicit conversion operator to a BYTE pointer.
-		*
-		* @return A pointer to the internal buffer.
-		*/
+    /**
+     * @brief Implicit conversion to raw buffer pointer.
+     *
+     * Intended for convenience when interfacing with low-level APIs.
+     */
 	operator BYTE* () const { return this->_buffer.get(); }
 
-	/**
-		* @brief Retrieves the size of the memory block being copied.
-		*
-		* @return The size of the memory block in bytes.
-		*/
+    /**
+     * @brief Get the size of the cached memory block.
+     */
 	size_t getSize() const { return this->_size; }
 
-	/**
-		* @brief Reads a portion of the internal buffer into an external buffer.
-		*
-		* @param[in] offset The starting offset within the internal buffer.
-		* @param[in] size The number of bytes to copy.
-		* @param[out] buffer A pointer to the destination buffer where the data will be copied.
-		* @return `true` if the read operation succeeds, `false` otherwise.
-		*
-		* @note This function fails if `offset + size` exceeds the internal buffer size.
-		*/
+    /**
+     * @brief Copy data from the internal buffer to an external buffer.
+     *
+     * @param[in] offset Byte offset within the internal buffer.
+     * @param[in] size   Number of bytes to copy.
+     * @param[out] buffer Destination buffer.
+     *
+     * @return True if the operation succeeds.
+     */
 	bool readFromBuffer(uint64_t offset, size_t size, void* buffer) const;
 
-	/**
-	* @brief Writes data from an external buffer into the internal buffer.
-	*
-	* @param[in] offset The starting offset within the internal buffer.
-	* @param[in] size The number of bytes to copy.
-	* @param[in] buffer A pointer to the source buffer containing the data to write.
-	* @return `true` if the write operation succeeds, `false` otherwise.
-	*
-	* @note This function fails if `offset + size` exceeds the internal buffer size.
-	*/
+    /**
+     * @brief Copy data from an external buffer into the internal buffer.
+     *
+     * @param[in] offset Byte offset within the internal buffer.
+     * @param[in] size   Number of bytes to copy.
+     * @param[in] buffer Source buffer.
+     *
+     * @return True if the operation succeeds.
+     */
 	bool writeToBuffer(uint64_t offset, size_t size, void* buffer);
 
-	/**
-	* @brief Retrieves a value of type `val` from the internal buffer at the given offset.
-	*
-	* @tparam val The type of value to read.
-	* @param[in] offset The offset within the internal buffer.
-	* @return The retrieved value, or `val()` if `offset + sizeof(val)` exceeds the buffer size.
-	*/
+    /**
+     * @brief Read a typed value from the internal buffer.
+     *
+     * @tparam T Value type.
+     * @param[in] offset Byte offset within the internal buffer.
+     *
+     * @return The value, or T{} if out of bounds.
+     */
 	template <class val>
 	val get(uint64_t offset) const;
 
-	/**
-	* @brief Writes a value of type `val` into the internal buffer at the given offset.
-	*
-	* @tparam val The type of value to write.
-	* @param[in] offset The offset within the internal buffer.
-	* @param[in] value The value to write.
-	* @return `true` if the write operation succeeds, `false` otherwise.
-	*
-	* @note This function fails if `offset + sizeof(val)` exceeds the buffer size.
-	*/
+    /**
+     * @brief Write a typed value into the internal buffer.
+     *
+     * @tparam T Value type.
+     * @param[in] offset Byte offset within the internal buffer.
+     * @param[in] value  Value to write.
+     *
+     * @return True if the write succeeds.
+     */
 	template <class val>
 	bool set(uint64_t offset, val value);
 
-	/**
-	* @brief Translates a pointer to the internal buffer into the original memory address.
-	*
-	* @param[in] ptr A pointer to a location within the internal buffer.
-	* @return The original memory address corresponding to `ptr`, or `0` if `ptr` is outside the buffer range.
-	*/
+    //==================================================================
+    // Address translation
+    //==================================================================
+
+    /**
+     * @brief Translate a buffer pointer back to its original remote address.
+     *
+     * @param[in] ptr Pointer into the internal buffer.
+     *
+     * @return Corresponding address in the target process, or 0 if invalid.
+     */
 	uint64_t translate(const void* ptr) const;
 
-	/**
-	* @brief Translates multiple pointers to the internal buffer into their original memory addresses.
-	*
-	* @param[in] ptrs A vector of pointers, each pointing to a location within the internal buffer.
-	* @return A vector of original memory addresses corresponding to each pointer. Any out-of-bounds pointers are translated to `0`.
-	*/
+    /**
+     * @brief Translate multiple buffer pointers back to remote addresses.
+     *
+     * @param[in] ptrs Pointers into the internal buffer.
+     *
+     * @return Vector of translated addresses (0 for invalid pointers).
+     */
 	std::vector<uint64_t> translate(std::vector<void*> ptrs) const;
 
 private:
