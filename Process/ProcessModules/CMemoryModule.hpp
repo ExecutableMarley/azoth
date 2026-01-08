c9b73a5bfde1758dbecdd4031348e8c947f5a3ba
Marley <marley.arns@yahoo.de>
2026-01-08 15:23:01 +0100

    feat: Added restore Point logic to virtualProtect and virtualAllocate/virtualFree

--- Process/ProcessModules/CMemoryModule.hpp
+++ Process/ProcessModules/CMemoryModule.hpp
@@ -18,6 +18,8 @@ namespace Azoth
 
 
 class CProcess;
+
+
 /**
  * @brief Platform-independent interface for remote process memory manipulation.
  *
@@ -247,20 +249,102 @@ public:
 		return regionList;
 	}
 
-    /**
+private:
+
+     //Placeholder
+     bool setError(EPlatformError errorCode) { return errorCode == EPlatformError::Success; }
+
+     //Not needed with current design
+     bool addProtectExRestore(uint64_t protectAddr, size_t size, EMemoryProtection oldProtect)
+     {
+          auto it = _pageProtectRestoreMap.find(protectAddr);
+          if (it == _pageProtectRestoreMap.end())
+          {
+               _pageProtectRestoreMap[protectAddr] = ProtectRestorePoint(size, oldProtect);
+               return true;
+          }
+          return false;
+     }
+public:
+
+     //Todo: Update this comment to reflect the no overlap ruleset
+     /**
      * @brief Change the protection of a memory range.
      *
-     * @param addr        Base address of the range.
-     * @param size        Size of the range in bytes.
-     * @param newProtect  New protection flags.
-     * @param oldProtect  Optional output for the previous protection.
-     *
+     * @param addr            Base address of the range.
+     * @param size            Size of the range in bytes.
+     * @param newProtect      New protection flags.
+     * @param pOldProtection  Optional output for the previous protection.
+     * 
      * @return True if the protection change succeeded.
      */
-	bool virtualProtect(uint64_t addr, size_t size, EMemoryProtection newProtect, EMemoryProtection* oldProtect)
+	bool virtualProtect(uint64_t addr, size_t size, EMemoryProtection newProtect, EMemoryProtection* pOldProtection)
 	{
-		return _platformLink->virtualProtect(addr, size, newProtect, oldProtect);
+          if (size == 0 || addr + size < addr)
+          {
+               return setError(EPlatformError::InvalidArgument);
+          }
+
+          uint64_t end = addr + size;
+
+          auto it = _pageProtectRestoreMap.lower_bound(addr);
+
+          bool isExactMatch      = (it != _pageProtectRestoreMap.end() && it->first == addr);
+          bool shouldDeleteEntry = false;
+          bool shouldAddEntry    = false;
+
+          if (isExactMatch)
+          {
+               if (it->second.size != size) { return setError(EPlatformError::InvalidArgument); }
+
+               if (it->second.origProtect == newProtect) { shouldDeleteEntry = true; }
+          }
+          else
+          {
+               // Forward overlap
+               if (it != _pageProtectRestoreMap.end() && it->first < end)
+               {
+                    return setError(EPlatformError::InvalidArgument);
+               }
+
+               // Backward overlap
+               if (it != _pageProtectRestoreMap.begin())
+               {
+                    auto prev = std::prev(it);
+                    if (prev->first + prev->second.size > addr)
+                    {
+                         return setError(EPlatformError::InvalidArgument);
+                    }
+               }
+               shouldAddEntry = true;
+          }
+
+          EMemoryProtection oldProt;
+          if (!_platformLink->virtualProtect(addr, size, newProtect, &oldProt))
+          {
+               return false; // platform sets error
+          }
+
+          if (shouldDeleteEntry) _pageProtectRestoreMap.erase(it);
+          else if (shouldAddEntry) addProtectExRestore(addr, size, oldProt);
+
+          if (pOldProtection) *pOldProtection = oldProt;
+          return setError(EPlatformError::Success);
 	}
+
+private:
+
+     bool addAllocationRestoreEntry(uint64_t allocatedAddr, size_t allocationSize)
+     {
+          if (_allocRestoreMap.count(allocatedAddr) == 0)
+          {
+               _allocRestoreMap.insert(allocatedAddr, allocationSize);
+               return true;
+          }
+          //This should not happen normally?
+          return false;
+     }
+public:
 
     /**
      * @brief Allocate virtual memory in the process.
@@ -273,24 +357,93 @@ public:
      */
 	uint64_t virtualAllocate(uint64_t addr, size_t size, EMemoryProtection protection = EMemoryProtection::RWX)
 	{
-		return _platformLink->virtualAllocate(addr, size, protection);
+          uint64_t allocatedAddr = _platformLink->virtualAllocate(addr, size, protection);
+          if (allocatedAddr != NULL)
+          {
+               addAllocationRestoreEntry(allocatedAddr, size);
+          }
+
+          return allocatedAddr;
 	}
 
-    /**
+     //Todo: Update this comment to reflect the allowUntracked mechanics
+     /**
      * @brief Free previously allocated virtual memory.
      *
      * @param addr Base address of the allocation.
      *
      * @return True if the memory was successfully released.
      */
-	bool virtualFree(uint64_t addr)
+	bool virtualFree(uint64_t addr, bool allowUntracked = false)
 	{
-		return _platformLink->virtualFree(addr);
+          auto it = _allocRestoreMap.find(addr);
+          if (it != _allocRestoreMap.end())
+          {
+               const bool success = _platformLink->virtualFree(addr);
+               if (success)
+               {
+                    _allocRestoreMap.erase(it);
+               }
+               return success;
+          }
+
+          if (allowUntracked)
+          {
+               return _platformLink->virtualFree(addr);
+          }
+          return false; //No tracked entry
 	}
 
 private:
+    struct CodeRestorePoint
+    {
+        size_t size;
+        std::unique_ptr<BYTE[]> originalBytes;
+    };
+
+    struct ProtectRestorePoint
+    {
+        size_t size;
+        EMemoryProtection origProtect;
+    };
+
+    struct AllocRestorePoint
+    {
+        size_t size;
+    };
+
 	CProcess* _backPtr;
 	IPlatformLink* _platformLink;
+
+     std::map<uint64_t, CodeRestorePoint>    _codeMemoryRestoreMap;
+     std::map<uint64_t, ProtectRestorePoint> _pageProtectRestoreMap;
+     std::map<uint64_t, AllocRestorePoint>   _allocRestoreMap;
+
+     bool hasOverlap(const std::map<uint64_t, ProtectRestorePoint>& map, uint64_t addr, size_t size)
+     {
+          if (size == 0 || map.empty())
+               return false;
+
+          uint64_t end = addr + size;
+          if (end < addr) //Overflow
+               return true;
+
+          auto it = map.lower_bound(addr);
+          if (it != map.end())
+          {
+              uint64_t start = it->first;
+              if (start < end)
+                  return true;
+          }
+          if (it != map.begin())
+          {
+               auto prev = std::prev(it);
+               uint64_t prevEnd = prev->first + prev->second.size;
+               if (prevEnd > addr)
+                    return true;
+          }
+          return false;
+     }
 };
 
 
