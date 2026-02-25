9a40e62111835084c4350ae66d8ce8d67fe82673
Marley <marley.arns@yahoo.de>
2026-02-25 15:06:46 +0100

    refactor: Moved some bigger methods to source file.

--- Process/ProcessModules/CMemoryModule.hpp
+++ Process/ProcessModules/CMemoryModule.hpp
@@ -253,31 +253,7 @@ public:
      *
      * @return A vector containing all matching memory regions.
      */
-     std::vector<MemoryRegion> queryAllMemoryRegions(Address startAddr, Address maxAddr, const MemoryRegionFilter& filter = {})
-     {
-          std::vector<MemoryRegion> regionList;
-          for (Address i = startAddr; i < maxAddr;)
-          {
-               MemoryRegion mr;
-               if (queryMemory(i, mr))
-               {
-                    if (mr.state == EMemoryState::Committed)
-                    {
-                         // Optionally filter regions
-                         if (!filter || filter(mr))
-                         {
-                              regionList.push_back(mr);
-                         }
-                    }
-                    i = mr.baseAddress + mr.regionSize;
-               }
-               else
-               {
-                    break;
-               }
-         }
-         return regionList;
-     }
+     std::vector<MemoryRegion> queryAllMemoryRegions(Address startAddr, Address maxAddr, const MemoryRegionFilter& filter = {});
 
      /**
      * @brief Query all memory regions within an address range.
@@ -338,63 +314,8 @@ public:
      * 
      * @return True if the protection change succeeded. On failure, an appropriate
      *         EPlatformError is set.
-     * 
-     * @note Passing a size of zero, an overflowing range, or any overlapping range
-     *       results in EPlatformError::InvalidArgument.
      */
-	bool virtualProtect(Address addr, size_t size, EMemoryProtection newProtect, EMemoryProtection* pOldProtection = nullptr)
-	{
-          if (size == 0 || addr + size < addr)
-          {
-               return setError(EPlatformError::InvalidArgument);
-          }
-
-          Address end = addr + size;
-
-          auto it = _pageProtectRestoreMap.lower_bound(addr);
-
-          bool isExactMatch      = (it != _pageProtectRestoreMap.end() && it->first == addr);
-          bool shouldDeleteEntry = false;
-          bool shouldAddEntry    = false;
-
-          if (isExactMatch)
-          {
-               if (it->second.size != size) { return setError(EPlatformError::RestorationViolation); }
-
-               if (it->second.origProtect == newProtect) { shouldDeleteEntry = true; }
-          }
-          else
-          {
-               // Forward overlap
-               if (it != _pageProtectRestoreMap.end() && it->first < end)
-               {
-                    return setError(EPlatformError::RestorationViolation);
-               }
-
-               // Backward overlap
-               if (it != _pageProtectRestoreMap.begin())
-               {
-                    auto prev = std::prev(it);
-                    if (prev->first + prev->second.size > addr)
-                    {
-                         return setError(EPlatformError::RestorationViolation);
-                    }
-               }
-               shouldAddEntry = true;
-          }
-
-          EMemoryProtection oldProt;
-          if (!_platformLink->virtualProtect(addr, size, newProtect, &oldProt))
-          {
-               return false; // platform sets error
-          }
-
-          if (shouldDeleteEntry) _pageProtectRestoreMap.erase(it);
-          else if (shouldAddEntry) addProtectExRestore(addr, size, oldProt);
-
-          if (pOldProtection) *pOldProtection = oldProt;
-          return setError(EPlatformError::Success);
-	}
+	bool virtualProtect(Address addr, size_t size, EMemoryProtection newProtect, EMemoryProtection* pOldProtection = nullptr);
 
      /**
       * @brief Restore the original protection of a tracked memory range.
@@ -410,21 +331,7 @@ public:
       * @return True if the protection was successfully restored or if no tracked
       *         entry exists, false if the platform call fails.
       */
-     bool restoreProtection(Address addr)
-     {
-          auto it = _pageProtectRestoreMap.find(addr);
-          if (it == _pageProtectRestoreMap.end())
-               return true;
-          
-          const auto& entry = it->second;
-
-          if (!_platformLink->virtualProtect(addr, entry.size, entry.origProtect, nullptr))
-               return false;
-          
-          _pageProtectRestoreMap.erase(it);
-          return true;
-     }
-
+     bool restoreProtection(Address addr);
 private:
 
      bool addAllocationRestoreEntry(Address allocatedAddr, size_t allocationSize)
@@ -490,25 +397,7 @@ public:
       *         untracked and `allowUntracked` is false, or if the platform call fails.
       *         On failure, an appropriate EPlatformError is set.
       */
-	bool virtualFree(Address addr, bool allowUntracked = false)
-	{
-          auto it = _allocRestoreMap.find(addr);
-          if (it != _allocRestoreMap.end())
-          {
-               const bool success = _platformLink->virtualFree(addr);
-               if (success)
-               {
-                    _allocRestoreMap.erase(it);
-               }
-               return success;
-          }
-
-          if (allowUntracked)
-          {
-               return _platformLink->virtualFree(addr);
-          }
-          return setError(EPlatformError::InvalidArgument);
-	}
+	bool virtualFree(Address addr, bool allowUntracked = false);
 
      /**
       * @brief Restore (free) a tracked virtual memory allocation.
@@ -525,88 +414,12 @@ public:
 
 private:
 
-     bool addMemoryRestorePoint(Address addr, size_t size)
-     {
-          if (size == 0 || addr + size < addr)
-               return setError(EPlatformError::InvalidArgument);
-
-          Address end = addr + size;
-          auto it = _codeMemoryRestoreMap.lower_bound(addr);
-
-          // Exact match
-          if (it != _codeMemoryRestoreMap.end() && it->first == addr)
-          {
-               if (it->second.size != size)
-                    return setError(EPlatformError::RestorationViolation);
-
-               return setError(EPlatformError::Success);
-          }
-
-          // Forward overlap
-          if (it != _codeMemoryRestoreMap.end() && it->first < end)
-               return setError(EPlatformError::RestorationViolation);
-
-          // Backward overlap
-          if (it != _codeMemoryRestoreMap.begin())
-          {
-               auto prev = std::prev(it);
-               if (prev->first + prev->second.size > addr)
-                    return setError(EPlatformError::RestorationViolation);
-          }
-
-          // Capture original bytes
-          auto original = std::make_unique<uint8_t[]>(size);
-          if (!read(addr, size, original.get()))
-               return false;
-
-          _codeMemoryRestoreMap.emplace(addr, CodeRestorePoint{ size, std::move(original) });
-
-	     return setError(EPlatformError::Success);
-     }
+     bool addMemoryRestorePoint(Address addr, size_t size);
 public:
 
-     bool patchReadOnlyMemory(Address addr, size_t size, const BYTE* buffer)
-     {
-          MemoryRegion mr;
-          if (!queryMemory(addr, mr)) return false;
+     bool patchReadOnlyMemory(Address addr, size_t size, const BYTE* buffer);
 
-          EMemoryProtection tmpProtection = mr.protection | EMemoryProtection::Write;
-
-          EMemoryProtection oldProtection;
-          if (!virtualProtect(mr.baseAddress, mr.regionSize, tmpProtection, &oldProtection))
-          {
-               return false;
-          }
-
-          if (!addMemoryRestorePoint(addr, size))
-          {
-               //Todo: This can overwrite the last Error
-               virtualProtect(mr.baseAddress, mr.regionSize, oldProtection, NULL);
-               return false;
-          }
-
-          bool success = this->write(addr, size, buffer);
-
-          //Todo: This also can overwrite the last Error
-          virtualProtect(mr.baseAddress, mr.regionSize, oldProtection, NULL);
-
-          return success;
-     }
-
-     bool restoreReadOnlyMemory(Address addr)
-     {
-          auto it = _codeMemoryRestoreMap.find(addr);
-          if (it == _codeMemoryRestoreMap.end())
-               return true;
-          
-          const auto& entry = it->second;
-
-          if (!write(addr, entry.size, entry.originalBytes.get()))
-               return false;
-          
-          _codeMemoryRestoreMap.erase(it);
-          return true;
-     }
+     bool restoreReadOnlyMemory(Address addr);
 
 private:
     struct CodeRestorePoint
